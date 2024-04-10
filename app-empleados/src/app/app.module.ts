ced0ec763cf21e7a4fd54d98d22098a00612361b
Rugren <rubgarciaruiz@gmail.com>
2024-04-10 09:53:56 +0200

    Creada barra navegación html y componentes varios (antes del routing)
    - Creada barra de navegación en HTML
    - Y creados los componentes que permitan movernos por por la aplicación, estos son:
    + HomeComponent
    + ProyectosComponent
    + quienesComponent
    + contactoComponent
    
    Antes de hacer el Routing.

--- app-empleados/src/app/app.module.ts
+++ app-empleados/src/app/app.module.ts
@@ -8,12 +8,20 @@ import { EmpleadoHijoCComponent } from './empleado-hijo-c/empleado-hijo-c.compon
 import { CaracteristicasEmpleadoCComponent } from './caracteristicas-empleado-c/caracteristicas-empleado-c.component';
 import { ServicioEmpleadosService } from './servicio-empleados.service';
 import { EmpleadosService } from './empleados.service';
+import { ProyectosComponentComponent } from './proyectos-component/proyectos-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { QuienesComponentComponent } from './quienes-component/quienes-component.component';
+import { ContactoComponentComponent } from './contacto-component/contacto-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoHijoCComponent,
-    CaracteristicasEmpleadoCComponent
+    CaracteristicasEmpleadoCComponent,
+    ProyectosComponentComponent,
+    HomeComponentComponent,
+    QuienesComponentComponent,
+    ContactoComponentComponent
   ],
   imports: [
     BrowserModule,
